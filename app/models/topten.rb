8ce600f714ae1a9f6e6427e62f721d8a88c93da9
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-03-09 02:34:39 +0530

    ssl problem in api

--- app/models/topten.rb
+++ app/models/topten.rb
@@ -5,7 +5,7 @@ class Topten
   def self.get_topten(category)
   	category = category
   	response = open("http://nielsen.api.tibco.com/TopTen/v1/"+category+"?apikey=2581-f9a6e0e1-c1bb-4ad9-988d-8ca5453c368d").read()
-  	result = JSON.parse(data)
-  	topten = response['Category']['category']
+  	result = JSON.parse(response)
+  	topten = result['Category']['category']
   end
 end
